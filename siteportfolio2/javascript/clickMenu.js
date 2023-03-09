bb6e55977c0739c7e67ec473e16a4433fd3e600f
Nicolas Abadia Pinheiro <62776448+Nicolas-Abadia@users.noreply.github.com>
2023-03-09 19:45:18 -0300

    Update clickMenu.js

--- siteportfolio2/javascript/clickMenu.js
+++ siteportfolio2/javascript/clickMenu.js
@@ -1,5 +1,5 @@
 function clickMenu() {
-    if (menu.style.display == 'flex') {
+    if (menu.style.display == 'flex' || window.matchMedia("(max-width: 972px)")) {
         menu.style.display = 'none'    
     } else {
         menu.style.display = 'flex'
